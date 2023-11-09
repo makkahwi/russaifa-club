5156085c7c182f8e9149c3e14789b52246258271
makkahwi <suhaibahmadai@hotmail.com>
2023-11-09 13:02:15 +0300

    Add Title To PageSection

--- src/components/PageSection/index.tsx
+++ src/components/PageSection/index.tsx
@@ -3,16 +3,19 @@ import { Row } from "reactstrap";
 interface props {
   children: React.ReactNode;
   color?: string;
+  title?: string;
 }
 
-const PageSection = ({ children = <></>, color = "light" }: props) => {
+const PageSection = ({ children = <></>, color = "white", title }: props) => {
   return (
     <Row
       style={{
         minHeight: "90vh",
       }}
-      className={`bg-${color} px-5`}
+      className={`bg-${color} px-5 py-3`}
     >
+      {title && <h1 className="my-5 pt-5 text-center">{title}</h1>}
+
       {children}
     </Row>
   );
