34949cf176205e6f48c03cce9fc5606e7d0fbf06
makkahwi <suhaibahmadai@hotmail.com>
2023-11-20 18:10:41 +0300

    Update TrustedBy Section

--- src/sections/About/TrustedBy.tsx
+++ src/sections/About/TrustedBy.tsx
@@ -7,21 +7,25 @@ const TrustedBySection = () => {
     { logo: "2.jpg", link: "Dubai" },
     { logo: "3.jpg", link: "Dubai" },
     { logo: "4.jpg", link: "Dubai" },
-    { logo: "5.jpg", link: "Dubai" },
-    { logo: "6.jpg", link: "Dubai" },
-    { logo: "7.jpg", link: "Dubai" },
-    { logo: "8.jpg", link: "Dubai" },
-    { logo: "9.jpg", link: "Dubai" },
-    { logo: "10.jpg", link: "Dubai" },
-    { logo: "11.jpg", link: "Dubai" },
-    { logo: "12.jpg", link: "Dubai" },
-    { logo: "13.jpg", link: "Dubai" },
-    { logo: "14.jpg", link: "Dubai" },
+    // { logo: "5.jpg", link: "Dubai" },
+    // { logo: "6.jpg", link: "Dubai" },
+    // { logo: "7.jpg", link: "Dubai" },
+    // { logo: "8.jpg", link: "Dubai" },
+    // { logo: "9.jpg", link: "Dubai" },
+    // { logo: "10.jpg", link: "Dubai" },
+    // { logo: "11.jpg", link: "Dubai" },
+    // { logo: "12.jpg", link: "Dubai" },
+    // { logo: "13.jpg", link: "Dubai" },
+    // { logo: "14.jpg", link: "Dubai" },
   ];
 
   return (
     <PageSection title="Trusted By" noMinHeight>
-      <Col md={12}></Col>
+      {partners.map(({ logo, link }, i) => (
+        <Col md={3} className="p-5" key={i}>
+          <img src={`/images/partners/${logo}`} width="100%" />
+        </Col>
+      ))}
     </PageSection>
   );
 };
