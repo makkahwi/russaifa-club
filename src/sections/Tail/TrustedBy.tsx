eb3768eed3518ce09e24f009a900bb6554b4b802
makkahwi <suhaibahmadai@hotmail.com>
2024-01-14 10:39:37 +0300

    Update TrsutedBy UI

--- src/sections/Tail/TrustedBy.tsx
+++ src/sections/Tail/TrustedBy.tsx
@@ -9,23 +9,29 @@ const TrustedBySection = () => {
     { logo: "merck.png", link: "https://www.merckgroup.com" },
     { logo: "joddb.png", link: "https://www.joddb.com/" },
     { logo: "real.jpg", link: "https://realcosmetics.jo/" },
-    { logo: "johnson.png", link: "https://www.jnj.com/" },
-    { logo: "giz.png", link: "https://www.giz.de/en" },
-    { logo: "gp.png", link: "https://www.gpstrategies.com/" },
-    { logo: "kempinski.png", link: "https://www.kempinski.com/" },
-    { logo: "pfizer.png", link: "https://www.pfizer.com/" },
-    { logo: "tabuk.png", link: "https://www.tabukpharmaceuticals.com/" },
-    { logo: "usaid.png", link: "https://www.usaid.gov/jordan" },
+    { logo: "johnson.png", link: "https://www.jnj.com/", bigger: true },
+    { logo: "giz.png", link: "https://www.giz.de/en", bigger: true },
+    { logo: "gp.png", link: "https://www.gpstrategies.com/", bigger: true },
+    { logo: "kempinski.png", link: "https://www.kempinski.com/", bigger: true },
+    { logo: "pfizer.png", link: "https://www.pfizer.com/", bigger: true },
+    {
+      logo: "tabuk.png",
+      link: "https://www.tabukpharmaceuticals.com/",
+      bigger: true,
+    },
+    { logo: "usaid.png", link: "https://www.usaid.gov/jordan", bigger: true },
+    { logo: "ccl.png", link: "https://www.ccl.org/", bigger: true },
     {
       logo: "zain.png",
       link: "https://www.jo.zain.com",
+      bigger: true,
     },
   ];
 
   return (
     <PageSection title="Trusted By" noMinHeight>
-      {partners.map(({ logo, link }, i) => (
-        <Col xs={6} md={6} lg={4} xl={2} className="p-5" key={i}>
+      {partners.map(({ logo, link, bigger }, i) => (
+        <Col xs={6} md={6} lg={4} xl={bigger ? 3 : 2} className="p-5" key={i}>
           <a href={link} target="_blank">
             <img src={`/images/partners/${logo}`} width="100%" />
           </a>
