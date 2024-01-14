8b90aba8f299335d59560ef602e7a3e07bb72ef1
makkahwi <suhaibahmadai@hotmail.com>
2024-01-14 10:58:02 +0300

    Build Numbers Counter

--- src/sections/Intro/Intro.tsx
+++ src/sections/Intro/Intro.tsx
@@ -1,6 +1,7 @@
 import PageSection from "@/components/PageSection";
 import Image from "next/image";
 import { Col, Row } from "reactstrap";
+import CountUp from "react-countup";
 
 const AboutIntroSection = () => {
   const stats = [
@@ -34,7 +35,9 @@ const AboutIntroSection = () => {
         <Row className="justify-content-between py-auto px-5">
           {stats.map(({ title, count }, i) => (
             <Col md={12} lg={3} className="my-auto text-center my-5" key={i}>
-              <h1 className="mt-4 text-primary">{count}</h1>
+              <h1 className="mt-4 text-primary">
+                <CountUp start={0} end={count} duration={3} enableScrollSpy />
+              </h1>
               <h5 className="mt-2 text-dark">{title}</h5>
             </Col>
           ))}
