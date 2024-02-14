5127e153518b738978d87e3733352edc36f1cca9
makkahwi <suhaibahmadai@hotmail.com>
2024-02-14 16:34:56 +0300

    Add Logo To Intro Section

--- src/sections/Intro/Intro.tsx
+++ src/sections/Intro/Intro.tsx
@@ -1,7 +1,7 @@
 import PageSection from "@/components/PageSection";
 import Image from "next/image";
-import { Col, Row } from "reactstrap";
 import CountUp from "react-countup";
+import { Col, Row } from "reactstrap";
 
 const AboutIntroSection = () => {
   const stats = [
@@ -33,14 +33,26 @@ const AboutIntroSection = () => {
         </h4>
 
         <Row className="justify-content-between py-auto px-5">
-          {stats.map(({ title, count }, i) => (
-            <Col xs={12} md={3} className="my-auto text-center my-5" key={i}>
-              <h1 className="mt-4 text-primary">
-                <CountUp start={0} end={count} duration={3} enableScrollSpy />
-              </h1>
-              <h5 className="mt-2 text-dark">{title}</h5>
-            </Col>
-          ))}
+          {stats.map(({ title, count }, i) => {
+            const size = (0.35 * String(count).length) / 5;
+
+            return (
+              <Col xs={12} md={3} className="my-auto text-center my-5" key={i}>
+                <Image
+                  src="/images/logo/sun.png"
+                  className="my-2"
+                  alt="Sun"
+                  width={409 * size}
+                  height={244 * size}
+                  priority
+                />
+                <h1 className="text-primary">
+                  <CountUp start={0} end={count} duration={3} enableScrollSpy />
+                </h1>
+                <h5 className="mt-2 text-dark">{title}</h5>
+              </Col>
+            );
+          })}
         </Row>
       </Col>
     </PageSection>
