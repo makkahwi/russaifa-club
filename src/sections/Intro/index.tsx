a76d2c7537a65323c59a08026e84560e485e0f14
makkahwi <suhaibahmadai@hotmail.com>
2024-02-19 20:21:04 +0300

    Replace Gallery With 1 Photo

--- src/sections/Intro/index.tsx
+++ src/sections/Intro/index.tsx
@@ -12,7 +12,19 @@ const IntroSections = () => {
 
       <WhoAmISection />
 
-      <GallerySection />
+      {/* <GallerySection /> */}
+
+      <div
+        style={{
+          height: "75vh",
+          backgroundImage: `url('/images/gallery/d.jpg')`,
+          backgroundPositionX: "center",
+          backgroundPositionY: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+        className="w-100 p-0 m-0"
+      />
     </Fragment>
   );
 };
