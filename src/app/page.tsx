ce22aeb1558284fac1443d1c18a4ce4c7b79be39
makkahwi <suhaibahmadai@hotmail.com>
2024-02-20 12:19:49 +0300

    Remove Dynamic Rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,15 @@
 "use client";
+
 import BookNowButton from "@/components/BookNowButton";
-import ScrollUpButton from "@/components/ScrollUp";
-import dynamic from "next/dynamic";
+import BusinessFacilitationSections from "@/sections/BusinessFacilitation";
+import CoachingSections from "@/sections/Coaching";
+import FooterSection from "@/sections/Footer";
+import HeaderSection from "@/sections/Header";
+import IntroSections from "@/sections/Intro";
+import TailSections from "@/sections/Tail";
 import { Fragment } from "react";
 
 const HomePage = () => {
-  const TailSections = dynamic(() => import("@/sections/Tail"));
-  const IntroSections = dynamic(() => import("@/sections/Intro"));
-  const HeaderSection = dynamic(() => import("@/sections/Header"));
-  const FooterSection = dynamic(() => import("@/sections/Footer"));
-  const CoachingSections = dynamic(() => import("@/sections/Coaching"));
-  const BusinessFacilitationSections = dynamic(
-    () => import("@/sections/BusinessFacilitation")
-  );
-
   return (
     <Fragment>
       <HeaderSection />
