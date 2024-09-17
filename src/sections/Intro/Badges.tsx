6640320cab457b5952c143cdd603dabe6daa2ef2
makkahwi <suhaibahmadai@hotmail.com>
2024-09-17 10:27:01 +0300

    Fix Badges UI

--- src/sections/Intro/Badges.tsx
+++ src/sections/Intro/Badges.tsx
@@ -1,59 +1,41 @@
-import Slider from "@/components/Slider";
-
 const BadgesSection = () => {
   const images = [
     {
-      src: "Actee.png",
-      width: 6,
+      src: "AqAi.png",
     },
     {
-      src: "AqAi.png",
-      width: 6,
+      src: "Actee.png",
     },
     {
       src: "DG1.png",
-      width: 6,
     },
     {
       src: "DG2.png",
-      width: 6,
     },
     {
       src: "icf.png",
-      width: 6,
-    },
-    {
-      src: "lego.png",
-      width: 6,
     },
     {
       src: "LND.png",
-      width: 6,
+    },
+    {
+      src: "lego.png",
     },
     {
       src: "SA.png",
-      width: 6,
     },
   ];
 
   return (
-    <Slider
-      duration={2500}
-      slides={images.map(({ src }, i) => (
-        <div
-          style={{
-            height: "25vh",
-            backgroundImage: `url('/images/badges/${src}')`,
-            backgroundPositionX: "center",
-            backgroundPositionY: "center",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-          }}
+    <div className="row bg-light">
+      {images.map(({ src }, i) => (
+        <img
+          src={`/images/badges/${src}`}
           key={i}
-          className="w-100 p-0 m-0"
+          className="col-3 col-md-1 px-2 my-3 mx-auto"
         />
       ))}
-    />
+    </div>
   );
 };
 
